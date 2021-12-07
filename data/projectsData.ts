2202f8ffc276f4ed2499a01d45c4fddbcf285e71
Carsten Noetzel <carsten.noetzel@gmail.com>
2021-12-07 17:44:59 +0100

    New blog posts and updated projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Nesting-Box Live-Stream',
+    description: `The live stream of my nesting box is public available. Feel free to have a look.`,
+    imgSrc: '/static/blog/2021-12-07-nesting-box.PNG',
+    href: 'http://nestingbox.divisionby0.de',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'How To build your own nesting box',
+    description: `If you want to build your own nesting box with IR-camera a how-to can by found in my blog.`,
+    imgSrc: '/static/brood2018/01-20180413114739-00.jpg',
+    href: '../blog/2019-07-11--Nesting-box-camera',
   },
 ]
 
